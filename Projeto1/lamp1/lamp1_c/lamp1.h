8f06ee0773378a14cea2500fad3db4932e5ffdc5
lhaislla <lhaislla@gmail.com>
2019-10-20 15:40:10 -0400

    tmc

---
+++ Projeto1/lamp1/lamp1_c/lamp1.h
@@ -0,0 +1,37 @@
+/* --- Generated the 2/10/2019 at 10:19 --- */
+/* --- heptagon compiler, version 1.03.00 (compiled thu. may. 3 2:35:29 CET 2018) --- */
+/* --- Command line: /usr/local/bin/heptc -target c -target z3z -s task lamp1.ept --- */
+
+#ifndef LAMP1_H
+#define LAMP1_H
+
+#include "lamp1_types.h"
+typedef struct Lamp1__lamp_mem {
+  int ck_1_2;
+  int pnr;
+} Lamp1__lamp_mem;
+
+typedef struct Lamp1__lamp_out {
+  int s;
+} Lamp1__lamp_out;
+
+void Lamp1__lamp_reset(Lamp1__lamp_mem* self);
+
+void Lamp1__lamp_step(int c, int b, Lamp1__lamp_out* _out,
+                      Lamp1__lamp_mem* self);
+
+typedef struct Lamp1__task_mem {
+  int ck_1;
+  int pnr;
+} Lamp1__task_mem;
+
+typedef struct Lamp1__task_out {
+  int s;
+} Lamp1__task_out;
+
+void Lamp1__task_reset(Lamp1__task_mem* self);
+
+void Lamp1__task_step(int c1, int b1, Lamp1__task_out* _out,
+                      Lamp1__task_mem* self);
+
+#endif // LAMP1_H
--- Projeto1/lamp3/lamp3_c/lamp3.h
+++ Projeto1/lamp3/lamp3_c/lamp3.h
@@ -1,12 +1,12 @@
-/* --- Generated the 14/10/2019 at 10:7 --- */
+/* --- Generated the 20/10/2019 at 15:17 --- */
 /* --- heptagon compiler, version 1.03.00 (compiled thu. may. 3 2:35:29 CET 2018) --- */
-/* --- Command line: /usr/local/bin/heptc -target c -target z3z -s task lamp3.ept --- */
+/* --- Command line: /usr/local/bin/heptc -target c -target z3z -s twotasks lamp3.ept --- */
 
 #ifndef LAMP3_H
 #define LAMP3_H
 
 #include "lamp3_types.h"
-#include "task_controller.h"
+#include "twotasks_controller.h"
 typedef struct Lamp3__lamp_mem {
   int ck_1_2;
   int pnr;
@@ -21,24 +21,24 @@ void Lamp3__lamp_reset(Lamp3__lamp_mem* self);
 void Lamp3__lamp_step(int c, int b, Lamp3__lamp_out* _out,
                       Lamp3__lamp_mem* self);
 
-typedef struct Lamp3__task_mem {
+typedef struct Lamp3__twotasks_mem {
   int ck_5_1;
   int ck_3_1;
   int ck_1;
   int pnr_2;
   int pnr_1;
   int pnr;
-} Lamp3__task_mem;
+} Lamp3__twotasks_mem;
 
-typedef struct Lamp3__task_out {
+typedef struct Lamp3__twotasks_out {
   int s1;
   int s2;
   int s3;
-} Lamp3__task_out;
+} Lamp3__twotasks_out;
 
-void Lamp3__task_reset(Lamp3__task_mem* self);
+void Lamp3__twotasks_reset(Lamp3__twotasks_mem* self);
 
-void Lamp3__task_step(int b1, int b2, int b3, Lamp3__task_out* _out,
-                      Lamp3__task_mem* self);
+void Lamp3__twotasks_step(int b1, int b2, int b3, Lamp3__twotasks_out* _out,
+                          Lamp3__twotasks_mem* self);
 
 #endif // LAMP3_H
